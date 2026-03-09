dde71a86d149a38c8035d8549d0c2e8dd9ad1a0a
jeffreysparker-collab <jeffreysparker@gmail.com>
2026-03-08 20:44:14 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () =>
-    navigator.serviceWorker.register('./sw.js').catch(console.error)
+   navigator.serviceWorker.register('/MOT_Circle_v2/sw.js').catch(console.error)
   );
 }
